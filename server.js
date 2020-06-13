45980b8763214ba98ea07a1b3de3ee660d362eed
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-06-13 15:42:10 +0700

    fix server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose
   .then(() => console.log("Connection Successfully"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("views/build"));
+  app.use(app.static("views/build"));
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views", "build", "index.html"));
